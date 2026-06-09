9ef77265e5a1d929bd2d72a1b49430cffd39d0ab
Harsh Mevada <harshmevada03@gmail.com>
2026-06-09 10:52:41 +0530

    debug true

--- sms/settings.py
+++ sms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6d4gt&zg%n8(@qd1@=x0lcnrn%49q@r(xd1=fv6rs#8zmll^zj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 
