604233d2991064e8499227a9238889552ff0c6ea
Harsh Mevada <harshmevada03@gmail.com>
2026-07-03 18:38:56 +0530

    solve query - school attendance zone set not showing time_rule

--- sms_app/serializer.py
+++ sms_app/serializer.py
@@ -2256,11 +2256,9 @@ class GetStudentSerializer(serializers.ModelSerializer):
 
 
 class AttendanceLocationSerializer(serializers.ModelSerializer):
-    start_time = serializers.TimeField(write_only=True, required=False, allow_null=True)
-    end_time = serializers.TimeField(write_only=True, required=False, allow_null=True)
-    half_day_time = serializers.TimeField(
-        write_only=True, required=False, allow_null=True
-    )
+    start_time = serializers.TimeField(source='time_rule.start_time',required=False, allow_null=True)
+    end_time = serializers.TimeField(source='time_rule.end_time',required=False, allow_null=True)
+    half_day_time = serializers.TimeField(source='time_rule.half_day_time',required=False, allow_null=True)
 
     class Meta:
         model = AttendanceLocation
@@ -2296,18 +2294,19 @@ class AttendanceLocationSerializer(serializers.ModelSerializer):
         if not school:
             raise serializers.ValidationError("User school is not configured.")
 
-        start_time = validated_data.pop("start_time", None)
-        end_time = validated_data.pop("end_time", None)
-        half_day_time = validated_data.pop("half_day_time", None)
+        
+        time_rule_data = validated_data.pop("time_rule")
+            
 
-        AttendanceTimeRule.objects.create(
+        time_rule = AttendanceTimeRule.objects.create(
             school=school,
-            start_time=start_time,
-            end_time=end_time,
-            half_day_time=half_day_time,
-        )
+            start_time=time_rule_data.get("start_time"),
+            end_time=time_rule_data.get("end_time"),
+            half_day_time=time_rule_data.get("half_day_time"),
+        )        
 
-        return AttendanceLocation.objects.create(school=school, **validated_data)
+        
+        return AttendanceLocation.objects.create(school=school, time_rule= time_rule, **validated_data)
 
 
 def is_inside_radius(lat1, lon1, lat2, lon2, radius_meters):
