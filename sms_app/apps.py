0ad5cf22a58b617ef5dddbb0ba3222491276afdc
Sujal Gujar <sujalgurjar919@gmail.com>
2026-08-14 22:08:15 +0530

    Auto-execute create_admin on post_migrate signal during deployment

--- sms_app/apps.py
+++ sms_app/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def auto_seed_on_migrate(sender, **kwargs):
+    from django.core.management import call_command
+    try:
+        call_command("create_admin")
+    except Exception as e:
+        print("Auto seed error:", e)
 
 class SmsAppConfig(AppConfig):
     name = "sms_app"
@@ -9,3 +16,4 @@ class SmsAppConfig(AppConfig):
             import sms_app.signals
         except ImportError:
             pass
+        post_migrate.connect(auto_seed_on_migrate, sender=self)
