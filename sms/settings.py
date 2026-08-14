88bdfa89aa5617983eaa33ce4f586fd4d8821c77
Sujal Gujar <sujalgurjar919@gmail.com>
2026-08-14 20:15:36 +0530

    Configure settings for production deployment on Render

--- sms/settings.py
+++ sms/settings.py
@@ -191,9 +191,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import dj_database_url
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.parse(DATABASE_URL)
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 # DATABASES = {
 #     'default': dj_database_url.config(
 #         default=os.environ.get("DATABASE_URL"),
@@ -243,6 +252,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
